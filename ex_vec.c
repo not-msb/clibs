03ec4563f3cb759054c5b278771937e5d3d9b8fb
MSB246 <msembulut@gmail.com>
2023-05-21 11:47:16 +0200

    I'm done commiting for now. Fixed some shitty C syntax. Thanks for making a '<char>' an int...

--- ex_vec.c
+++ ex_vec.c
@@ -2,23 +2,24 @@
 #include <stdint.h>
 #include "vec.h"
 
+VEC_DEFINE(char);
 VEC_DEFINE(int);
 
 int main() {
     Bumper bumper = binit();
 
-    Vec_int v1 = VEC_NEW(int, &bumper);
+    Vec_char v1 = VEC_NEW(char, &bumper);
     printf("Length before: %zu\n", v1.length);
-    printf("%ls\n", v1.ptr);
+    printf("%s\n", v1.ptr);
 
-    VEC_PUSH(v1, 'h');
-    VEC_PUSH(v1, 'e');
-    VEC_PUSH(v1, 'l');
-    VEC_PUSH(v1, 'l');
-    VEC_PUSH(v1, 'o');
-    VEC_PUSH(v1, '\0');
+    VEC_PUSH(v1, (char)'h');
+    VEC_PUSH(v1, (char)'e');
+    VEC_PUSH(v1, (char)'l');
+    VEC_PUSH(v1, (char)'l');
+    VEC_PUSH(v1, (char)'o');
+    VEC_PUSH(v1, (char)'\0');
     printf("Length after:  %zu\n", v1.length);
-    printf("%ls\n", v1.ptr);
+    printf("%s\n", v1.ptr);
 
     for (size_t i = v1.length; i>0; i--) {
         char c = VEC_POP(v1);
